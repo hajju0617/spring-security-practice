73a2a592c5aac089e7cce10966b02f260ed0785b
hajju0617 <joowon023617@gmail.com>
2025-01-17 23:39:54 +0900

    Spring Security 연습해보기

---
+++ src/main/java/com/project/springsecuritypractice/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.project.springsecuritypractice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // throws Exception 적는 이유 : authorizeHttpRequests, build가 예외를 던지고 있음.
+        httpSecurity
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
+    }
+}
